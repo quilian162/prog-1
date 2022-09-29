2c91e71c1e139146c057e53f8794634793c909b9
Quilian Augusto Moretti <83377444+quilian162@users.noreply.github.com>
2022-09-29 17:10:59 -0300

    Create 3.java

---
+++ 3.java
@@ -0,0 +1,9 @@
+public class Main {
+public static void main(String args[]) {
+    int x, soma;
+    for(x=1; x<=10; x++){
+        soma = x * 5;
+        System.out.printf("%d x 5 = %d\n",x ,soma); 
+    }
+}
+}
