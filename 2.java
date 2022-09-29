973f58113ac1f5ab6ad775ec2f6867f6b871fe03
Quilian Augusto Moretti <83377444+quilian162@users.noreply.github.com>
2022-09-29 16:59:44 -0300

    Create 2.java

---
+++ 2.java
@@ -0,0 +1,11 @@
+public class Main {
+public static void main(String args[]) {
+    int x = 5, y = 7, soma;
+    double a = 9, b = 15, soma2;
+
+    soma = (x + y);
+    soma2 = (a + b);
+    System.out.println("A soma de inteiros é: " +soma);
+    System.out.println( "A soma de doubles é: " +soma2);
+    }
+}
