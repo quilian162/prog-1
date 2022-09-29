bfac2eff93b13193786b9f4045f8cdca2b4de325
Quilian Augusto Moretti <83377444+quilian162@users.noreply.github.com>
2022-09-29 16:32:02 -0300

    Create 1.java

---
+++ 1.java
@@ -0,0 +1,5 @@
+public class Main {
+public static void main(String args[]) {
+    System.out.println("Programação I\n -  Professora: Andressa Sebben\n -  Aluno: Quilian Augusto Moretti\n -  Objetivo: O objetivo dessa disciplina é compreender os conceitos fundamentais do paradigma de programação orientada a objetos e aplicá-los no desenvolvimento de soluções de software.\n -  Turno: Noturno");   
+    }
+}
