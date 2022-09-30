9da3c88e33838fcac21cc7c4958e78204a62b70a
Quilian Augusto Moretti <83377444+quilian162@users.noreply.github.com>
2022-09-29 21:51:47 -0300

    Create 6.java

---
+++ 6.java
@@ -0,0 +1,23 @@
+public class Ex3{
+    public static void main(String[] args){
+        int count = 0;
+        for (int j = 2; j < 58; j++){
+            boolean primo = true;
+            if(count == 15){
+                break;
+            }
+        
+        for(int x =2; x<j;x++){
+            if (j % x == 0){
+                primo = false;   
+            }
+        }
+
+        if(primo){
+        System.out.println(j);
+            count++;
+        }
+    }
+    }
+}
+
