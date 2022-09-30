f99b068e21c5e6d5ca9639b2baf8bfcbf4b5ec18
Quilian Augusto Moretti <83377444+quilian162@users.noreply.github.com>
2022-09-29 21:55:05 -0300

    Create 7.java

---
+++ 7.java
@@ -0,0 +1,18 @@
+public class Ex3{
+    public static void main(String[] args){
+        int count = 0;
+        for (int j = 100; j < 200; j++){
+            boolean primo = true;
+        
+        for(int x =2; x<j;x++){
+            if (j % x == 0){
+                primo = false;   
+            }
+        }
+
+        if(primo){
+        System.out.println(j);
+        }
+    }
+    }
+}
