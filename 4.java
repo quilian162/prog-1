6d1f3cb2f72bf53539cd4a5e06273ec073cbbad9
Quilian Augusto Moretti <83377444+quilian162@users.noreply.github.com>
2022-09-29 21:23:54 -0300

    Create 4.java

---
+++ 4.java
@@ -0,0 +1,9 @@
+public class Ex3{
+    public static void main(String[] args){
+        int i;
+        for(i=1; i<11; i++){
+            System.out.print(i + "x" + 6 + " = " +i*6);
+            System.out.println("\t\t" + i + "x" + 2 + " = " +i*2);
+        }
+    }
+}
