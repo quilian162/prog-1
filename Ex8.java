3e1ab26c2b0d95221f0e079bcbf732ab99e4e245
Quilian Augusto Moretti <83377444+quilian162@users.noreply.github.com>
2022-09-29 22:25:32 -0300

    Update and rename 8.java to Ex8.java

--- Ex8.java
+++ Ex8.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Main{
+public class Ex8{
     public static void main(String[] args){
         Scanner scan = new Scanner(System.in);
         int[] vet = new int[10]; 
