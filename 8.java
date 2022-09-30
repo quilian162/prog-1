9372b86a3d24a3d5f2db154e2a8e7ba51f0f51e4
Quilian Augusto Moretti <83377444+quilian162@users.noreply.github.com>
2022-09-29 22:22:56 -0300

    Update 8.java

--- 8.java
+++ 8.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Ex3{
+public class Main{
     public static void main(String[] args){
         Scanner scan = new Scanner(System.in);
         int[] vet = new int[10]; 
