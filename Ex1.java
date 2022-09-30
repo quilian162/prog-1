554fb3977f4fa7face575a7da94433e3307164b4
Quilian Augusto Moretti <83377444+quilian162@users.noreply.github.com>
2022-09-29 22:23:28 -0300

    Update and rename 1.java to Ex1.java

--- Ex1.java
+++ Ex1.java
@@ -1,4 +1,4 @@
-public class Main {
+public class Ex1{
 public static void main(String args[]) {
     System.out.println("Programação I\n -  Professora: Andressa Sebben\n -  Aluno: Quilian Augusto Moretti\n -  Objetivo: O objetivo dessa disciplina é compreender os conceitos fundamentais do paradigma de programação orientada a objetos e aplicá-los no desenvolvimento de soluções de software.\n -  Turno: Noturno");   
     }
