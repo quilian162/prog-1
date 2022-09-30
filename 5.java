50659bf944455274c51fe9742a2ddb7f4b8e4ebe
Quilian Augusto Moretti <83377444+quilian162@users.noreply.github.com>
2022-09-29 21:34:11 -0300

    Create 5.java

---
+++ 5.java
@@ -0,0 +1,10 @@
+public class Ex3{
+    public static void main(String[] args){
+        String string = " atrapalham ";
+        int count;
+        System.out.println("1 Java atrapalha muita gente");
+        for(count=2; count<=100; count++){
+            System.out.println(count + " Java " + string.repeat(count) + "atrapalham muita gente");
+        }
+    }
+}
